65bb3dcb52801f4bc2ece80a22052389b665778e
cjqed <christopherjonesqed@gmail.com>
2014-07-28 00:38:00 -0400

    Initial attempt at jaro winkler

--- src/distance.rs
+++ src/distance.rs
@@ -1,18 +1,79 @@
-struct calc_objects<'a> {
+struct CalcObjects<'a> {
   first: &'a str,
   second: &'a str
 }
-pub fn jaro_winkler_distance<'sl>(str1: &'sl str, str2: &'sl str) -> f64 {
-  let co = calc_objects{ first: str1, second: str2 };
+pub fn jaro_winkler_distance<'sl>(str1: &'sl str, str2: &'sl str) -> f32 {
+  let co = CalcObjects{ first: str1, second: str2 };
   let m = get_num_matching_chars(&co);
   let t = get_num_transpositions(&co) / 2.0;
-  0.0
+  if m < 1.0 {
+    0.0
+  }
+  else {
+    let f1: f32 = m / str1.len() as f32;
+    let f2: f32 = m / str2.len() as f32;
+    let f3: f32 = (m - t) / m;
+    ( ( f1 + f2 + f3 ) / 3.0 )
+  }
 }
 
-fn get_num_matching_chars(obj: &calc_objects) -> f64 {
-  0.0
+fn get_num_matching_chars(obj: &CalcObjects) -> f32 {
+  let mut m = 0.0;
+  
+  for (i,c) in obj.first.chars().enumerate() {
+    if i >= obj.second.len() {
+      break;
+    }
+    if c == obj.second.char_at(i) {
+      m += 1.0;
+    }
+  }
+  m
 }
 
-fn get_num_transpositions(obj: &calc_objects) -> f64 {
-  0.0
+fn get_num_transpositions(obj: &CalcObjects) -> f32 {
+  let mut t = 0.0;
+  let max_length =
+    if obj.first.len() > obj.second.len() {
+      obj.first.len()
+    }
+    else {
+      obj.second.len()
+    };
+  let match_buffer = ( max_length / 2 ) - 1;
+  
+  for (i,c) in obj.first.chars().enumerate() {
+    if i >= obj.second.len() {
+      break;
+    }
+    if c != obj.second.char_at(i) {
+      if is_transposed(obj.second, i, match_buffer, c) {
+        t += 1.0;
+      }
+    }
+  }
+  t
+}
+
+fn is_transposed<'a>(word: &'a str, index: uint, buffer: uint, matching_char: char) -> bool {
+  let start_index: uint = if index < buffer { 
+      0 
+    } 
+    else { 
+      index - buffer 
+    };
+    
+  let end_index: uint = if word.len() >= index + buffer { 
+      word.len() - 1 
+    }
+    else { 
+      index + buffer 
+    };
+
+  for (i,c) in word.slice(start_index,end_index).chars().enumerate() {
+    if c == matching_char {
+      return true;
+    }
+  }
+  return false;
 }
--- tests/distance_tests.rs
+++ tests/distance_tests.rs
@@ -4,14 +4,14 @@ use natural::distance::jaro_winkler_distance;
 
 #[test]
 fn test_jaro_winkler() {
-  f64_eq(jaro_winkler_distance("one", "one"), 1.0);
-  f64_eq(jaro_winkler_distance("one", "one"), 1f64);
-  f64_eq(jaro_winkler_distance("one", ""), 0f64);
-  f64_eq(jaro_winkler_distance("", ""), 0f64);
-  f64_eq(jaro_winkler_distance("", "one"), 0f64);
-  f64_eq(jaro_winkler_distance("dixon", "dicksonx"), 0.746666f64);  
+  f64_eq(jaro_winkler_distance("one", "one"), 1f32);
+  f64_eq(jaro_winkler_distance("one", ""), 0f32);
+  f64_eq(jaro_winkler_distance("", ""), 0f32);
+  f64_eq(jaro_winkler_distance("", "one"), 0f32);
+  println!("{}", jaro_winkler_distance("DWAYNE", "DUANE"));
+  f64_eq(jaro_winkler_distance("DWAYNE", "DUANE"), 0.822);  
 }
 
-fn f64_eq(a: f64, b: f64) {
-  assert!((a - b).abs() < 0.01);
+fn f64_eq(a: f32, b: f32) {
+  assert!((a - b).abs() < 0.03);
 }
