8e7fd2306f7cffe345ad2a28d3dcb536d85194b7
cjqed <christopherjonesqed@gmail.com>
2014-07-29 00:29:54 -0400

    Jaro winkler distance finished

--- src/distance.rs
+++ src/distance.rs
@@ -1,79 +1,96 @@
 struct CalcObjects<'a> {
   first: &'a str,
-  second: &'a str
+  second: &'a str,
 }
-pub fn jaro_winkler_distance<'sl>(str1: &'sl str, str2: &'sl str) -> f32 {
-  let co = CalcObjects{ first: str1, second: str2 };
-  let m = get_num_matching_chars(&co);
-  let t = get_num_transpositions(&co) / 2.0;
-  if m < 1.0 {
-    0.0
+
+impl<'a> CalcObjects<'a> {
+  fn max_length(&self) -> uint {
+    return if self.first.len() > self.second.len() {
+      self.first.len()
+    }
+    else {
+      self.second.len()
+    };
   }
-  else {
-    let f1: f32 = m / str1.len() as f32;
-    let f2: f32 = m / str2.len() as f32;
-    let f3: f32 = (m - t) / m;
+}
+
+struct JaroWinkler<'a> {
+  co: CalcObjects<'a>,
+  matches1: Vec<char>,
+  matches2: Vec<char>
+}
+
+impl<'b> JaroWinkler<'b> {
+  fn new<'b> (this_obj: CalcObjects<'b>) -> JaroWinkler<'b> {
+    let mut jw = JaroWinkler {
+      co: this_obj,
+      matches1: Vec::new(),
+      matches2: Vec::new()
+    };
+    jw.matches1 = jw.get_common_chars(false);
+    jw.matches2 = jw.get_common_chars(true);
+    jw
+  }
+  
+  fn get_common_chars(&self, ordering: bool) -> Vec<char> {
+    let mut common_chars = Vec::new();
+    let max_length = self.co.max_length();
+    let match_buffer = ( max_length / 2 ) - 1;
+    let mut f = self.co.first;
+    let mut s = self.co.second;
+    if ordering {
+      f = self.co.second;
+      s = self.co.first;
+    }
+    
+    if f.len() == 0 || s.len() == 0 {
+      return common_chars;
+    }
+    
+    for (i,c) in f.chars().enumerate() {
+      let s_index: uint = if i < match_buffer {
+        0
+      }
+      else {
+        i - match_buffer
+      };
+      let e_index: uint = if s.len() <= i + match_buffer { 
+        s.len() 
+      }
+      else { 
+        i + match_buffer + 1
+      };
+      let word_slice = s.slice(s_index, e_index);
+      if word_slice.contains_char(c) {
+        common_chars.push(c);
+      }
+    }
+    common_chars
+  }
+  
+  fn calculate(&self) -> f32 {
+    if self.matches1.len() == 0 || self.matches2.len() == 0 {
+      return 0.0;
+    }
+    if self.matches1.len() != self.matches2.len() {
+      return 0.0;
+    }
+    
+    let mut t = 0.0;
+    let n = self.matches1.len();
+    for i in range(0u,n) {
+      if self.matches2[i] != self.matches1[i] {
+        t += 0.5;
+      }
+    }
+    let f1: f32 = self.matches1.len() as f32 / self.co.first.len() as f32;
+    let f2: f32 = self.matches2.len() as f32 / self.co.second.len() as f32;
+    let f3: f32 = (self.matches1.len() as f32 - t) / self.matches1.len() as f32;
     ( ( f1 + f2 + f3 ) / 3.0 )
   }
 }
 
-fn get_num_matching_chars(obj: &CalcObjects) -> f32 {
-  let mut m = 0.0;
-  
-  for (i,c) in obj.first.chars().enumerate() {
-    if i >= obj.second.len() {
-      break;
-    }
-    if c == obj.second.char_at(i) {
-      m += 1.0;
-    }
-  }
-  m
-}
-
-fn get_num_transpositions(obj: &CalcObjects) -> f32 {
-  let mut t = 0.0;
-  let max_length =
-    if obj.first.len() > obj.second.len() {
-      obj.first.len()
-    }
-    else {
-      obj.second.len()
-    };
-  let match_buffer = ( max_length / 2 ) - 1;
-  
-  for (i,c) in obj.first.chars().enumerate() {
-    if i >= obj.second.len() {
-      break;
-    }
-    if c != obj.second.char_at(i) {
-      if is_transposed(obj.second, i, match_buffer, c) {
-        t += 1.0;
-      }
-    }
-  }
-  t
-}
-
-fn is_transposed<'a>(word: &'a str, index: uint, buffer: uint, matching_char: char) -> bool {
-  let start_index: uint = if index < buffer { 
-      0 
-    } 
-    else { 
-      index - buffer 
-    };
-    
-  let end_index: uint = if word.len() >= index + buffer { 
-      word.len() - 1 
-    }
-    else { 
-      index + buffer 
-    };
-
-  for (i,c) in word.slice(start_index,end_index).chars().enumerate() {
-    if c == matching_char {
-      return true;
-    }
-  }
-  return false;
+pub fn jaro_winkler_distance<'sl>(str1: &'sl str, str2: &'sl str) -> f32 {
+  let jw = JaroWinkler::new(CalcObjects{ first: str1, second: str2 });
+  jw.calculate()
 }
--- tests/distance_tests.rs
+++ tests/distance_tests.rs
@@ -8,8 +8,9 @@ fn test_jaro_winkler() {
   f64_eq(jaro_winkler_distance("one", ""), 0f32);
   f64_eq(jaro_winkler_distance("", ""), 0f32);
   f64_eq(jaro_winkler_distance("", "one"), 0f32);
-  println!("{}", jaro_winkler_distance("DWAYNE", "DUANE"));
-  f64_eq(jaro_winkler_distance("DWAYNE", "DUANE"), 0.822);  
+  f64_eq(jaro_winkler_distance("DWAYNE", "DUANE"), 0.822);
+  f64_eq(jaro_winkler_distance("Martha", "Marhta"), 0.944);
+  f64_eq(jaro_winkler_distance("dixon", "dicksonx"), 0.767);
 }
 
 fn f64_eq(a: f32, b: f32) {
