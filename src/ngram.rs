01039cf73d1a28c8b32e074d3309c57d09bb3f07
cjqed <christopherjonesqed@gmail.com>
2014-07-31 00:17:47 -0400

    ngram complete

--- src/ngram.rs
+++ src/ngram.rs
@@ -18,18 +18,35 @@ impl<'a> NGram<'a> {
     
     //left-padding
     if self.pad != "" {
-    
+      for i in range(1, self.n) {
+        let num_blanks = self.n - i;
+        let mut this_sequence = Vec::new();
+        for _j in range(0, num_blanks) {
+          this_sequence.push(self.pad);
+        }
+        let sl = tokenized_sequence.slice(0, self.n-num_blanks);
+        ngram_result.push(this_sequence.append(sl));
+      }
     }
     
     //Fill the rest of the ngram
     for i in range(0, count) {
-      let sl = Vec::from_slice(tokenized_sequence.slice(i,i+self.n));
+      let a = tokenized_sequence.slice(i,i+self.n);
+      let sl = Vec::from_slice(a);
       ngram_result.push(sl);
     }
     
     //right-padding
     if self.pad != "" {
-    
+      for i in range(1, self.n) {
+        let num_blanks = i;
+        let last_entry = tokenized_sequence.len();
+        let mut tc = Vec::new().append(tokenized_sequence.slice(last_entry - num_blanks, last_entry));
+        for _j in range(0, num_blanks) {
+          tc.push(self.pad);
+        }
+        ngram_result.push(tc);
+      }
     }
     ngram_result
   }
@@ -40,6 +57,7 @@ pub fn get_ngram<'a>(this_text: &'a str, this_n: uint) -> Vec<Vec<&'a str>> {
   ng.calculate()
 }
 
-pub fn get_ngram_with_padding<'a>(this_text: &'a str, this_n: uint, this_padding: &'a str)  {
-  
+pub fn get_ngram_with_padding<'a>(this_text: &'a str, this_n: uint, this_padding: &'a str) -> Vec<Vec<&'a str>> {
+  let ng = NGram { text: this_text, n: this_n, pad: this_padding };
+  ng.calculate()
 }
--- tests/ngram_tests.rs
+++ tests/ngram_tests.rs
@@ -1,9 +1,15 @@
 extern crate natural;
 
 use natural::ngram::get_ngram;
+use natural::ngram::get_ngram_with_padding;
 
 #[test]
 fn test_ngram() {
-  println!("{}", get_ngram("hello my darling", 2));
   assert_eq!(get_ngram("hello my darling", 2), vec![vec!["hello", "my"], vec!["my", "darling"]]);
+}
+
+#[test]
+fn test_ngram_with_pad() {
+  assert_eq!(get_ngram_with_padding("my fleas", 2, "boo"), vec![
+    vec!["boo", "my"], vec!["my", "fleas"], vec!["fleas", "boo"]]);
 }
