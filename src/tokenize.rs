527601442349eedd22be66a341c67a17830ee071
Mário Feroldi <euseinaoseithelost@gmail.com>
2016-01-27 06:48:36 -0200

    Fixed split issue

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,20 +1,17 @@
 use std::str::CharEq;
 
 pub fn tokenize(text: &str) -> Vec<&str> {
-  text.split(Splitter).filter(|s| s.len() > 0).collect()
+  text.split(Splitter::is_match).filter(|s| s.len() > 0).collect()
 }
 
 struct Splitter;
 
-impl CharEq for Splitter {
-    fn matches(&mut self, c: char) -> bool {
+impl Splitter {
+    fn is_match(c: char) -> bool {
         match c {
             ' ' | ',' | '.' | '!' | '?' | ';' | '\'' |  '"'
             | ':' | '\t' | '\n' | '(' | ')' | '-' => true,
             _ => false
         }
     }
-
-    // We're only matching ASCII chars, so we can use the faster impl.
-    fn only_ascii(&self) -> bool { true }
 }
