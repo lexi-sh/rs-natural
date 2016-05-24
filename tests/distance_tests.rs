3d55ce67c8af98666fde5cf5c367a78b84499f71
Craig Wilkinson <curlywurlycraig@gmail.com>
2016-05-24 10:08:11 -0700

    Remove needless tests

--- tests/distance_tests.rs
+++ tests/distance_tests.rs
@@ -10,15 +10,7 @@ fn test_jaro_winkler() {
   f64_eq(jaro_winkler_distance("", "one"), 0f32);
   f64_eq(jaro_winkler_distance("DWAYNE", "DUANE"), 0.822);
   f64_eq(jaro_winkler_distance("Martha", "Marhta"), 0.944);
-  f64_eq(jaro_winkler_distance("dixon", "dicksonx"), 0.767);
-}
-
-#[test]
-fn test_jaro_strings() {
-  f64_eq(jaro_winkler_distance("one", "one"), 1f32);
-  f64_eq(jaro_winkler_distance("one", "one"), 1f32);
-  f64_eq(jaro_winkler_distance("one", "one"), 1f32);
-  f64_eq(jaro_winkler_distance("one", "one"), 1f32);
+  f64_eq(jaro_winkler_distance("dicksonx", "dixon"), 0.767);
 }
 
 fn f64_eq(a: f32, b: f32) {
