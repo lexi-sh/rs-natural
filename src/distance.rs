005663d2991cb4c0dd40205c0953864352696219
Craig Wilkinson <curlywurlycraig@gmail.com>
2016-05-24 10:56:53 -0700

    Rewrite jaro-winkler to not be stateful

--- src/distance.rs
+++ src/distance.rs
@@ -1,95 +1,68 @@
+use std::cmp;
 
-struct CalcObjects<'a> {
-  first: &'a str,
-  second: &'a str,
-}
-
-impl<'a> CalcObjects<'a> {
-  fn max_length(&self) -> usize {
-    if self.first.len() > self.second.len() {
-      self.first.len()
+fn max_length(str1: &str, str2: &str) -> usize {
+    if str1.len() > str2.len() {
+      str1.len()
     } else {
-      self.second.len()
+      str2.len()
     }
-  }
 }
 
-struct JaroWinkler<'a> {
-  co: CalcObjects<'a>,
-  matches1: Vec<char>,
-  matches2: Vec<char>
-}
-
-impl<'b> JaroWinkler<'b> {
-  fn new(this_obj: CalcObjects<'b>) -> JaroWinkler<'b> {
-    let mut jw = JaroWinkler {
-      co: this_obj,
-      matches1: Vec::new(),
-      matches2: Vec::new()
-    };
-    jw.matches1 = jw.get_common_chars(false);
-    jw.matches2 = jw.get_common_chars(true);
-    jw
-  }
-  
-  fn get_common_chars(&self, ordering: bool) -> Vec<char> {
+fn get_common_chars(str1: &str, str2: &str, ordering: bool) -> Vec<char> {
     let mut common_chars = Vec::new();
-    let max_length = self.co.max_length();
+    let max_length = max_length(str1, str2);
     let match_buffer = ( max_length / 2 ) - 1;
-    let mut f = self.co.first;
-    let mut s = self.co.second;
-    
+    let mut f = str1;
+    let mut s = str2;
+
     if ordering {
-      f = self.co.second;
-      s = self.co.first;
+        f = str2;
+        s = str1;
     }
-    
+
     for (i, c) in f.chars().enumerate() {
-      let s_index: usize = if i < match_buffer {
-        0
-      } else {
-        i - match_buffer
-      };
+        let s_index: usize = if i < match_buffer {
+            0
+        } else {
+            i - match_buffer
+        };
       
-      let e_index: usize = if s.len() <= (i + match_buffer) { 
-        s.len() 
-      } else { 
-        i + match_buffer + 1
-      };
-      
-      let word_slice = &s[s_index..e_index];
-      if word_slice.contains(c) {
-        common_chars.push(c);
-      }
+        let e_index: usize = if s.len() <= (i + match_buffer) { 
+            s.len() 
+        } else { 
+            i + match_buffer + 1
+        };
+          
+        let word_slice = &s[s_index..e_index];
+        if word_slice.contains(c) {
+            common_chars.push(c);
+        }
     }
+
     common_chars
-  }
+}
   
-  fn calculate(&self) -> f32 {
-    // Should it really return?
-    if self.matches1.is_empty() || self.matches2.is_empty()
-      || self.matches1.len() != self.matches2.len()
+pub fn jaro_winkler_distance(str1: &str, str2: &str) -> f32 {
+    let matches1 = get_common_chars(str1, str2, false);
+    let matches2 = get_common_chars(str1, str2, true);
+
+    if matches1.is_empty() || matches2.is_empty()
+      || matches1.len() != matches2.len()
     {
       return 0.0;
     }
     
     let mut t = 0.0;
-    for (a, b) in self.matches1.iter().zip(self.matches2.iter()) {
+    for (a, b) in matches1.iter().zip(matches2.iter()) {
       if a != b {
         t += 0.5;
       }
     }
     
-    let f1 = self.matches1.len() as f32 / self.co.first.len() as f32;
-    let f2 = self.matches2.len() as f32 / self.co.second.len() as f32;
-    let f3 = (self.matches1.len() as f32 - t) / self.matches1.len() as f32;
+    let f1 = matches1.len() as f32 / str1.len() as f32;
+    let f2 = matches2.len() as f32 / str2.len() as f32;
+    let f3 = (matches1.len() as f32 - t) / matches1.len() as f32;
     (f1 + f2 + f3) / 3.0
-  }
-}
-
-pub fn jaro_winkler_distance(str1: &str, str2: &str) -> f32 {
-  let jw = JaroWinkler::new(CalcObjects { first: str1, second: str2 });
-  jw.calculate()
 }
 
 /* pub fn levenshtein_distance(a: &str, b: &str) -> usize {
